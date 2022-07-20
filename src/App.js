2d00004ec45f708f01433aa27f3ccbb7285beb11
VKB321 <vijaybhukya009@gmail.com>
2022-07-21 01:40:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter, Routes , Route} from "react-router-dom";
+import LandingPage from './Landing/LandingPage';
+import PostView from './PostView/PostView';
+import Post from './Post/Post';
+import Header from './Header/Header';
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/header' element={<Header/>}/>
+        <Route path='/postview' element={<PostView/>}/>
+      <Route path='/post' element={<Post/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
